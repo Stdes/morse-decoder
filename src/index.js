463d4890424c860cfe65a982612fd30733d7bb49
Stanislav <stanislav.msb@yandex.ru>
2023-09-11 08:44:23 +1000

    feat: change increment 10 to 8, add check morseCode !== "

--- src/index.js
+++ src/index.js
@@ -38,45 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-    // 10 === .
-    // 11 === -
-    // space === **********
 
-    let pair = ''    ;
+    let pair = '';
     let morseCode = '';
     let outputStr = '';
 
-    
-    for (let i = 0; i < expr.length+1; i = i + 2) {
+    for (let i = 0; i < expr.length+1; i += 2) {
 
         //если счетчик кратен 10, нужно определить символ алфавита и добавить его в строку
-        if ((i !== 0) && ((i % 10) === 0)) {
+        if ((i !== 0) && ((i % 10) === 0) && (morseCode !== '')) {
             outputStr = outputStr + MORSE_TABLE[morseCode];
             morseCode = '';
         }
 
         pair =  expr.slice (i, i+2);
-        if (pair === '10') {morseCode = morseCode + '.'};
-        if (pair === '11') {morseCode = morseCode + '-'};
+        if (pair === '10') {morseCode += '.'};
+        if (pair === '11') {morseCode += '-'};
         if (pair === '**') {//попался пробел
-            outputStr = outputStr + ' ';
-            i = i + 10; //скипаем последовательность **********
+            outputStr += ' ';
+            i += 8; //скипаем последовательность **********
         };
     };
+
     return outputStr;
 }
-
-/*
-проходят все тесты, кроме одного: 
-ожидается:
-+answer on the ultimate question of life the universe and everything is 42
-получено:
--answer on the ultimate question of life the universe and everything is v2
-
-то есть вместо 42 получаем v2 или вместо вместо ....- мы получаем ...-
-одна точка куда-то девается. Не понимаю, как при этом все 49 тестов проходят.
-*/
 
 module.exports = {
     decode
