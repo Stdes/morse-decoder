b68875f25497740c9fcbd4e02e73416b4c769e1d
Stanislav <stanislav.msb@yandex.ru>
2023-09-10 18:47:44 +1000

    feat: all tests passed except one: easy 10th

--- src/index.js
+++ src/index.js
@@ -39,6 +39,39 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    // 10 === .
+    // 11 === -
+    // space === **********
+
+    let pair = ''    ;
+    let morseCode = '';
+    let outputStr = '';
+
+   
+    for (let i = 0; i < expr.length+1; i = i + 2) {
+
+        //если счетчик кратен 10, значит нужно определить символ алфавита и добавить его в строку
+        if ((i !== 0) && ((i % 10) === 0)) {
+            outputStr = outputStr + MORSE_TABLE[morseCode];
+            morseCode = '';
+        }
+
+        pair =  expr.slice (i, i+2);
+        if (pair === '10') {morseCode = morseCode + '.'};
+        if (pair === '11') {morseCode = morseCode + '-'};
+        if (pair === '**') {//попался пробел
+            outputStr = outputStr + ' ';
+            i = i + 10; //скипаем последовательность **********
+        };
+
+        //вместо ....- мы получаем ...-
+
+    };
+
+    return outputStr;
+
+   
+   
 }
 
 module.exports = {
